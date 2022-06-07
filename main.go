f1a0df466dc218855e2485618dc25907202cbd6c
kurankat <dr.microbe@gmail.com>
2022-06-07 11:11:10 +1000

    Extract bioregion from JSON response

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
 var inputHeaders = []string{
 	"Locality Name",
@@ -43,13 +49,40 @@ type Request struct {
 }
 
 type Response struct {
-	field       string
-	description string
-	layername   string
-	pid         string
-	value       string
+	Field       string `json:"field"`
+	Description string `json:"description"`
+	Layername   string `json:"layername"`
+	Pid         string `json:"pid"`
+	Value       string `json:"value"`
 }
 
+var alaClient = &http.Client{}
+
 func main() {
-	fmt.Println("Main")
+	getBioregion("-39.93125", "143.85099")
+}
+
+func getJson(url string, target interface{}) error {
+	r, err := alaClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	jsonResp, _ := ioutil.ReadAll(r.Body)
+	stringResp := string(jsonResp)
+	trimmed1 := strings.ReplaceAll(stringResp, "[", "")
+	trimmed2 := strings.ReplaceAll(trimmed1, "]", "")
+
+	forDecoder := strings.NewReader(trimmed2)
+	return json.NewDecoder(forDecoder).Decode(target)
+}
+
+func getBioregion(lat, long string) (bioregion string) {
+	region := &Response{}
+
+	requestURL := fmt.Sprintf("https://spatial.ala.org.au/ws/intersect/1048/%s/%s", lat, long)
+	getJson(requestURL, region)
+
+	return region.Value
 }
--- main_test.go
+++ main_test.go
@@ -8,15 +8,6 @@ import (
 	"testing"
 )
 
-// equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
-	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
-	}
-}
-
 var testLocation = LocalityData{
 	localityName: "Currie",
 	country:      "Australia",
@@ -27,15 +18,15 @@ var testLocation = LocalityData{
 	datum:        "GDA94",
 }
 
-func Test_Main(t *testing.T) {
-	for key, value := range testLocation {
-		fmt.Printf("Header: %s,\tValue: %s", key, value)
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
 	}
-
-	equals(t, true, true)
 }
 
-// Locality Name	Variants	State	Country	Lat/Long Method	Latitude 1	Longitude 1	Datum
-// Currie		State of Tasmania	Australia	Decimal degrees	-39.93125	143.85099	GDA94
-
-// https://spatial.ala.org.au/ws/intersect/1048/-23.1/149.1
+func TestGetBioregion(t *testing.T) {
+	equals(t, "King", getBioregion(testLocation.lat, testLocation.long))
+}
